450da146ace18863be1136e35e06a6f2e26c32f8
defender11 <ch345@mail.ru>
2015-02-13 03:03:30 +0400

    31:07 php_week_3_day_1

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php 
-	print "Apache work";
-	phpinfo();
+	class Animal{
+		public $name;
+		public $age = 0;
 
-	class user{
-
+		function sayHello() {
+			echo $this->name." Hello";
+		}
 	}
- ?>
+
+	$cat = new Animal();
+	$dog = new Animal();
+
+	$cat->name = "Murzik";
+	$dog->name = "Tuzik";
+
+	echo $cat->name;
+	echo $dog->name;
+
+	$cat->sayHello();
+
+?>
